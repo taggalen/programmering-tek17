48f8fe7f3fe92cdd2885d696ccdd40e178426f83
taggalen <nasser.elbatalabdul@elev.ga.ntig.se>
2018-11-19 11:23:07 +0100

    Initial commit

---
+++ src/arrays/arrays28.java
@@ -0,0 +1,15 @@
+package arrays;
+
+public class arrays28 {
+
+	public static void main(String[] args)
+    {
+        String str = "Java Exercises.";
+
+        // Convert the above string to a char array.
+        char[] arr = str.toCharArray();
+
+        // Display the contents of the char array.
+        System.out.println(arr);
+    }
+}
---
+++ src/arrays/arrays30.java
@@ -0,0 +1,16 @@
+package arrays;
+
+public class arrays30 {
+
+	public static void main(String[] args)
+    {
+        String str = "The Quick BroWn FoX!";
+
+        // Convert the above string to all uppercase.
+        String upper_str = str.toUpperCase();
+
+        // Display the two strings for comparison.
+        System.out.println("Original String: " + str);
+        System.out.println("String in uppercase: " + upper_str);
+    }
+}
